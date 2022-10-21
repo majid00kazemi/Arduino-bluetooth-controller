35cdc2c017037f306925cd6d1172ba61304a604b
Majid Kazemi <majid00kazemi@gmail.com>
2022-10-21 19:23:51 +0330

    changed function

--- Controller.ino
+++ Controller.ino
@@ -1,26 +1,55 @@
+
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(4,2);
+
 const int relay = 7; 
-char msg,cmd;
+char msg;
+char c=' ';
+String data;
 void setup() {
   // Initialization
   pinMode(relay, OUTPUT);
-  pinMode(led1,OUTPUT);
-  pinMode(led2,OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600); 
+  BTserial.begin(9600);
   msg = "";
-  digitalWrite(led2,HIGH);
   digitalWrite(relay, HIGH); 
 }
 
 void loop() {
   int status = digitalRead(relay);
   
-  if (Serial.available() > 0){ 
-    msg = Serial.read();
+  while(BTserial.available()){
+    c = BTserial.read();
+    if(c != -1){
+      data +=c;
+      if(c=='\n'){
+        break;
+      }
+      
+    }
   }
-  if (msg == '1'){
-      digitalWrite(relay, LOW); 
-      Serial.print(" Opend\n"); 
-      msg = ""; 
-  } 
+
+if (data == "majid00kazemi@gmail.com"){
+          digitalWrite(relay, LOW);
+          Serial.print(" Opend by majid\n"); 
+          BTserial.write("opend bye bye.");
+          digitalWrite(LED_BUILTIN, HIGH); 
+          data="";
+        }
   digitalWrite(relay, HIGH);
+  digitalWrite(LED_BUILTIN, LOW); 
+
+
+    // if (BTserial.available())
+  // {  
+  //       c = BTserial.read();
+  //       Serial.write(c);  
+  //       if (c == "majid00kazemi@gmail.com"){
+  //         digitalWrite(relay, LOW);
+  //         Serial.print(" Opend by majid\n"); 
+  //         Serial.println();  
+  //         digitalWrite(LED_BUILTIN, HIGH); 
+  //       }
+  // }
 }
